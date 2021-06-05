a7cfd487ab9226ded6d546a39be56d883be7677a
nikokortizzion <nikokortizzion@users.noreply.github.com>
2021-06-05 10:18:45 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 # include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    string name;
+    name = get_string("Wat is your name?\n");
+    printf("Hello, %s\n", name);
 }
